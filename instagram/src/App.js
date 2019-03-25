56fdc517e088991a69416d92ee1b1735ccfc0eed
josephriosio <blankworldwide@gmail.com>
2019-03-25 12:14:50 -0700

    added barbones search bar

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
 
+// data for dummy users
+import dummyData from "./dummy-data";
+// search bar
+import SearchBar from "./components/SearchBar/SearchBar";
+
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: dummyData
+    };
+  }
   render() {
     return (
       <div className="App">
-        <h1>hi</h1>
+        <SearchBar />
       </div>
     );
   }
---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+// styling
+import "./SearchBar.css";
+
+const SearchBar = () => {
+  return (
+    <nav className="search-bar">
+      <h1> Instagram </h1>
+      <input />
+      <div className="buttons">
+        <a> button1 </a>
+        <a> button2 </a>
+        <a> button3 </a>
+      </div>
+    </nav>
+  );
+};
+
+export default SearchBar;
