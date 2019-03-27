5df0df9c44dc254795e714af627df464834ea851
josephriosio <blankworldwide@gmail.com>
2019-03-27 16:20:14 -0700

    added styled components

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -3,6 +3,60 @@ import "./login.css";
 import logo from "./logo.png";
 import logo0 from "./logo0.png";
 import bottom from "./bottom-info.png";
+import styled from "styled-components";
+
+// styled components
+const LoginDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginData = styled.div`
+  margin-left: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #fffafa;
+  height: 100%;
+
+  @media screen and (max-width: 500px) {
+    margin-left: 0;
+  }
+`;
+
+const Button = styled.button`
+  height: 30px;
+  background-color: #3897f0;
+  border: 1px solid #3897f0;
+  border-radius: 4px;
+  color: #fff;
+  position: relative;
+`;
+
+const MainP = styled.p`
+  padding-left: 3rem;
+  margin: 30px 0;
+  text-align: center;
+  color: #696969;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-flow: column;
+`;
+
+const Logo = styled.img`
+  padding-left: 5.8rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 4rem;
+  width: 10rem;
+  @media screen and (max-width: 500px) {
+    padding-left: 6.3rem;
+  }
+`;
 
 class Login extends Component {
   state = {
@@ -26,18 +80,16 @@ class Login extends Component {
 
   render() {
     return (
-      <div className="login">
+      <LoginDiv>
         <img className="side-image" src={logo} />
-        <div className="login-data">
-          <form className="user-input">
-            <img className="insta-logo" src={logo0} />
-            <p className="main top-info">
+        <LoginData className="login-data">
+          <Form>
+            <Logo src={logo0} />
+            <MainP className=" top-info">
               Sign up to see photos and videos from your friends.
-            </p>
-            <button className="login-btn" onClick={this.signIn}>
-              Log in with Facebook
-            </button>
-            <p className="main or">OR</p>
+            </MainP>
+            <Button onClick={this.signIn}>Log in with Facebook</Button>
+            <MainP className="or">OR</MainP>
             <input
               className="login-input"
               placeholder="Mobile Number or Email"
@@ -55,17 +107,15 @@ class Login extends Component {
               type="password"
               placeholder="password"
             />
-            <button className="login-btn" onClick={this.signIn}>
-              login
-            </button>
-            <p className="main top-info">
+            <Button onClick={this.signIn}>login</Button>
+            <MainP className=" top-info">
               By signing up, you agree to our Terms , Data Policy and Cookies
               Policy .
-            </p>
+            </MainP>
             <img src={bottom} />
-          </form>
-        </div>
-      </div>
+          </Form>
+        </LoginData>
+      </LoginDiv>
     );
   }
 }
