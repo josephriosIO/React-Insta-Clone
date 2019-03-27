5370936eb47b1260a78a7763052961ed9ee12824
josephriosio <blankworldwide@gmail.com>
2019-03-27 13:29:08 -0700

    added styling to desktop

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import "./login.css";
+import logo from "./logo.png";
+import logo0 from "./logo0.png";
+import bottom from "./bottom-info.png";
 
 class Login extends Component {
   state = {
@@ -22,17 +26,33 @@ class Login extends Component {
 
   render() {
     return (
-      <div>
-        <form>
-          <input
-            value={this.state.username}
-            onChange={this.handleChanges}
-            name="username"
-            placeholder="username"
-          />
-          <input placeholder="password" />
-          <button onClick={this.signIn}>login</button>
-        </form>
+      <div className="login">
+        <img src={logo} />
+        <div className="login-data">
+          <form className="user-input">
+            <img className="insta-logo" src={logo0} />
+            <p className="top-info">
+              Sign up to see photos and videos from your friends.
+            </p>
+            <button onClick={this.signIn}>Log in with Facebook</button>
+            <p className="or">OR</p>
+            <input placeholder="Mobile Number or Email" />
+            <input placeholder="Full Name" />
+            <input
+              value={this.state.username}
+              onChange={this.handleChanges}
+              name="username"
+              placeholder="username"
+            />
+            <input placeholder="password" />
+            <button onClick={this.signIn}>login</button>
+            <p className="top-info">
+              By signing up, you agree to our Terms , Data Policy and Cookies
+              Policy .
+            </p>
+            <img src={bottom} />
+          </form>
+        </div>
       </div>
     );
   }
