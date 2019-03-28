350d99466787a5b2385968cd4b8b9b5db0b535b2
Joseph <newuser@News-MacBook-Pro.local>
2019-03-27 17:08:35 -0700

    added logout button

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -4,12 +4,18 @@ import "./SearchBar.css";
 import logo from "./logo.png";
 
 const SearchBar = props => {
+  const signOut = () => {
+    localStorage.clear()
+  }
   return (
     <nav className="search-bar">
       <h1 className="logo">
         <i className="fab fa-instagram " /> | <img src={logo} alt="broken" />{" "}
       </h1>
-      <input type="text" onKeyDown={props.search} placeholder="Search" />
+      <form>
+      <input type="search" onKeyDown={props.search} placeholder="Search" />
+      <button onClick={signOut}>Logout</button>
+      </form>
       <div className="buttons">
         <i className="far fa-compass fa-2x" />
         <i className="far fa-heart fa-2x" />
